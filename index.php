949cc52df2edbca0606818ca259b8a1165ea158f
meidityaputra <95512061+meidityaputra@users.noreply.github.com>
2022-10-27 14:49:50 +0700

    Membuat UI Login dan signup

--- index.php
+++ index.php
@@ -9,9 +9,24 @@
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="./common/header.html">
     <link rel="stylesheet" href="./common/footer.html">
+    <title>Binotify</title>
 </head>
-
+    
 <body>
+    <div class="login-modal">
+        <div class="login-modal_logo">
+            <i class = "fab fa-binotify"></i>
+            <h2>Binotify</h2>
+        </div>
+        <div class="login-modal_info">
+            <p>You have to login to use this feature.</p>
+            <a href="./auth/login.php" class="login">Login</a>
+            <a href="./auth/signup.php" class="signup">Haven't create an account yet?</a>
+            <div class="close">+</div>
+        </div>
+
+
+    </div>
 
 
 </body>
