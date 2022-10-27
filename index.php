6723b0a677ad52d92205906921c9715f1ed73792
hasbialfarabi <hasbialfarabi9@gmail.com>
2022-10-27 08:30:13 +0700

    changing index type to .php

--- index.php
+++ index.php
@@ -4,13 +4,16 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Binotify</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="./common/header.html">
+    <link rel="stylesheet" href="./common/footer.html">
 </head>
 
 <body>
-    <h1></h1>
+
+
 </body>
 
 </html>
